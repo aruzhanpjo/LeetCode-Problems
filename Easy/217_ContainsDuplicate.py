4376b30bb6fa512024b7bf264e854cd7a51f33f2
aruzhanpjo <aruzhan.betigenova@mnsu.edu>
2023-07-13 21:06:56 -0500

    need to come back to this one for a review & concept retention

--- Easy/217_ContainsDuplicate.py
+++ Easy/217_ContainsDuplicate.py
@@ -3,10 +3,13 @@ Given an integer array nums, return true if any value
 appears at least twice in the array, and return false if every element is distinct.
 """
 
+#need to redo this one!
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # could not solve; learned about set().
-        
+    
+
         numSet=set()
 
         for i in nums:
